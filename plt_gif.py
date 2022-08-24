dc0d791e2b5a648c6a9957a0430bd347260cf9a2
Hbaskett <111974401+Hbaskett@users.noreply.github.com>
2022-08-24 12:09:49 +0100

    Update plt_gif.py
    added selector to chose which images to make into a gif

--- plt_gif.py
+++ plt_gif.py
@@ -10,7 +10,13 @@ import os
 #    path = os.path.join(parent_dir, directory)
 #    os.mkdir(path)
 
-# Used to produce gifs
+select = 1 # use this to select which gif you want to make 
+# 0 - wind contours
+# 1 PT structures
+# 2 species vertical profiles
+
+# make gifs
+
 images_MdH0 = []
 images_MdH1 = []
 
@@ -22,21 +28,38 @@ for mdh in ["MdH0","MdH1"]:
     if mdh == "MdH1":
         day_step = np.arange(10,710,10)
         images = images_MdH1
-    
-    for x in ["zonal","meridonal","vertical"]:
-        for y in ["zonal","meridonal","vertical"]:
+        
+    if select == 0:
+        
+        for x in ["zonal","meridonal","vertical"]:
+            for y in ["zonal","meridonal","vertical"]:
             
-            images.clear()
-            for day in day_step:
+                images.clear()
+                for day in day_step:
         
-                # Choose images to compile
-                image_dir = Path.home() / "um" / "um_runs" / "winds" / mdh / f"day_{day}"
-                image_file = f"{mdh}_{x}_mean_{y}_wind_{day}.png"
+                    # Choose images to compile
+                    image_dir = Path.home() / "um" / "um_runs" / "winds" / mdh / f"day_{day}"
+                    image_file = f"{mdh}_{x}_mean_{y}_wind_{day}.png"
         
-                # Choose name and output directory of final gif
-                gif_dir = Path.home() / "um" / "um_runs" / "winds" 
-                gif_file = f"{mdh}_{x}_mean_{y}_wind_total.gif"
-                images.append(imageio.imread(image_dir / image_file))
+                    # Choose name and output directory of final gif
+                    gif_dir = Path.home() / "um" / "um_runs" / "winds" 
+                    gif_file = f"{mdh}_{x}_mean_{y}_wind_total.gif"
+                    images.append(imageio.imread(image_dir / image_file))
         
-            imageio.mimsave(gif_dir / gif_file, images)
- 
+                imageio.mimsave(gif_dir / gif_file, images)
+                
+    if select == 1:
+    
+        images.clear()
+        for day in day_step:
+        
+            # Choose images to compile
+            image_dir = Path.home() / "um" / "um_runs" / "PT_lats" / mdh 
+            image_file = f"{mdh}_PT_{day}.png"
+        
+            # Choose name and output directory of final gif
+            gif_dir = Path.home() / "um" / "um_runs" / "PT_lats" / mdh 
+            gif_file = f"{mdh}_PT_total.gif"
+            images.append(imageio.imread(image_dir / image_file))
+        
+        imageio.mimsave(gif_dir / gif_file, images)
