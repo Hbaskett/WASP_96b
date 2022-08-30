599c8a137837e4242c740dc5248f57030da64d9c
Hbaskett <111974401+Hbaskett@users.noreply.github.com>
2022-08-30 11:03:13 +0100

    uses fcontour
    replaced pcolormesh with fcontour to so plots less pixelated, so also removed vmin/vmax selector. Added correct titles to match descriptions from Mayne 2017

--- plt_zonal_momentum_budget.py
+++ plt_zonal_momentum_budget.py
@@ -6,24 +6,20 @@ import iris
 import iris.pandas
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from iris.analysis.maths import apply_ufunc
 from aeolus.calc import deriv
-from aeolus.coord import interp_cube_from_height_to_pressure_levels, regrid_3d
+from aeolus.coord import regrid_3d
 from aeolus.model import um
 from aeolus.plot import add_custom_legend, subplot_label_generator
-#from util_commons import GASES, PLANETS, SUITES
-from HD209_commons import SUITES
-
-# Functions but not giving correct scale outputs ideas are:
-# - np.gradient wrong?
-# - maths need full radius but then how to plot in the range?
+from util_commons import GASES, PLANETS, SUITES
+#from HD209_commons import SUITES
 
 #Parameters
 warnings.filterwarnings("ignore", module="iris")
-planet = "hd209458b"
+planet = "wasp96b"
 exp = "equilibrium"
 metallicity = "solar"
 mdh = "MdH0"
+inner_r_wasp = 83893200.0
 spc_mdh0 = 3516.2947232694723
 spc_mdh1 = 3164.69353854304
 spc_HD209 =  3516.6
@@ -31,43 +27,33 @@ comp = "meridonal"
 moment = "momentum"
 limit = 0 # Use 0 to plot zoomed data set at jet or full data set
 t = 0 # time slice integer
-day = (t*10)+10
-tgt_plevs = np.logspace(7, 2, 65)  # target pressure levels
-output_dir = Path.home() / "um" / "um_runs" / "check_conservation" 
+day = (t*10)+10 # calculate the day to put into the figures titles
+output_dir = Path.home() / "um" / "um_runs" / "check_conservation" / "zonal_momentum_budget"
 
 # Process data
 vrbls = {}
 vrbls[planet] = {}
 for exp in ["equilibrium", "kinetics"]:
     vrbls[planet][exp] = {}
-    for metallicity in ["solar"]: #,"10xsolar"]:
+    for metallicity in ["solar","10xsolar"]:
         vrbls[planet][exp][metallicity] = {}
         
-        # Cannot have same time frame until all runs are finished then just specify t in parameters and remove if statements
-        if metallicity == "solar":
-            t=99
-            day = (t*10)+10
-        if metallicity == "10xsolar":
-            t=44
-            day = (t*10)+10
-        
-        # Load data
-        fpath = SUITES[planet][exp][metallicity]["dir_for_merged"]
-        fname = f"{SUITES[planet][exp][metallicity]['suite']}.nc"
-        u = iris.load_cube(str(fpath / fname), um.u)
-        v = iris.load_cube(str(fpath / fname), um.v)
-        w = iris.load_cube(str(fpath / fname), um.w)
-        pres = iris.load_cube(str(fpath / fname), "air_pressure")
-        temp = iris.load_cube(str(fpath / fname), "air_temperature")
-        
-        # Calculate density for each metallicity case
-        if metallicity == "solar":
-            spc = spc_HD209
+        # Cannot have same time frame until all runs are finished then just specify t in parameters
+        if metallicity == "solar"
+            spc = spc_mdh0
         if metallicity == "10xsolar":
             spc = spc_mdh1
-    
+        
+        # Load data and slice at a give t
+        fpath = SUITES[planet][exp][metallicity]["dir_for_merged"]
+        fname = f"{SUITES[planet][exp][metallicity]['suite']}.nc"
+        u = iris.load_cube(str(fpath / fname), um.u)[t,:,:,:]
+        v = iris.load_cube(str(fpath / fname), um.v)[t,:,:,:]
+        w = iris.load_cube(str(fpath / fname), um.w)[t,:,:,:]
+        pres = iris.load_cube(str(fpath / fname), "air_pressure")[t,:,:,:]
+        temp = iris.load_cube(str(fpath / fname), "air_temperature")[t,:,:,:]
         density = pres/(spc*temp)
-    
+        
         # fixes gridding bug by removing var_name from the cubes
         for cube in [u, v, w, density]:
             for coord_name in [um.z, um.x, um.y]:  # or um.y
@@ -77,18 +63,18 @@ for exp in ["equilibrium", "kinetics"]:
         # so need to regrid so they have the same grid values and can then be multiplied for the averages
         u_regrid = regrid_3d(u,density,um)
         v_regrid = regrid_3d(v,density,um)
-    
-        #slice at certain time and remove extra levels
-        u_space = u_regrid[t,:,:,:]
-        v_space = v_regrid[t,:,0:90,:]
-        w_space = w[t,:,:,:]
-        density_space = density[t,:,:,:]
-
+        
+        # load into maths variables
+        u_space = u_regrid
+        v_space = v_regrid
+        w_space = w
+        density_space = density
+        
         #extract space coordinates
         lons = w.coord("longitude").points # think this coordinate is redundant
         lats = w.coord("latitude").points
         lvl = w.coord("level_height").points
-        radius = w.coord("level_height").points + 9e7 #1.03e7 #add the inner radius to make up the full radius at each point 
+        radius = w.coord("level_height").points + inner_r_wasp #1.03e7 #add the inner radius to make up the full radius at each point 
         
         # Equations from Mayne 2017 section 3.3.2
         # calculate parts of the equations 
@@ -151,46 +137,32 @@ for exp in ["equilibrium", "kinetics"]:
             "total" : total_eddy,
         }
 
-for metallicity in ["solar"]: #,"10xsolar"]:
+for metallicity in ["solar","10xsolar"]:
 
     # selects the names of the output files
     if metallicity == "solar":
         mdh = "MdH0"
-        t=99
-        day = (t*10)+10
     if metallicity == "10xsolar":
         mdh = "MdH1"
-        t=44
-        day = (t*10)+10
-        
+       
     for moment in ["momentum","eddy"]:
         fig, axes = plt.subplots(
-            ncols=2, nrows=3, figsize=(15, 20), sharex=True, sharey=True, constrained_layout=True
+            ncols=2, nrows=3, figsize=(24, 30), sharex=True, sharey=True, constrained_layout=True
         )
         #axes[0,0].invert_yaxis()
         iletters = subplot_label_generator()
         for exp, axcol in zip(["equilibrium", "kinetics"], axes.T):
             for comp, ax in zip(["meridonal","vertical","total"], axcol):
             
-                # To normalise plots need to find the max and minimum of each data set then choose the larger magnitude and set vmax = -vmin or vmin = -vmax depending on which has a larger magnitude
-                data_max = vrbls[planet][exp][metallicity][moment][comp].data.max()
-                data_min = vrbls[planet][exp][metallicity][moment][comp].data.min()
-            
-                if abs(data_max) >= abs(data_min):
-                    norm_max = data_max
-                    norm_min = -data_max
-                else:
-                    norm_max = -data_min
-                    norm_min = data_min
-        
-                p0 = ax.pcolormesh(
+                p0 = ax.contourf(
                     lats,
                     lvl,
                     vrbls[planet][exp][metallicity][moment][comp].data,
-                    norm = mpl.colors.Normalize(vmin=norm_min, vmax=norm_max),
+                    levels=100, # argument which selects how many steps the colorbar is split into
+                    norm = mpl.colors.TwoSlopeNorm(vcenter = 0), # keeps 0 as white on contour
                     cmap="RdBu_r",
-                    shading="auto",
-                    rasterized=True,
+                    extend = 'both',
+                    alpha = 1,
                 )
             
                 p1 = ax.contour(
@@ -207,14 +179,31 @@ for metallicity in ["solar"]: #,"10xsolar"]:
                 fig.colorbar(
                     p0,
                     ax=ax,
-                    label = "Hi",
+                    #label = "Hi", # Don't know what to label the colorbar
                     orientation="horizontal",
                     #shrink=0.3,
+                    extend = "both"
                 )
-    
-                ax.set_title(f"{planet} {comp} {moment} budget for {metallicity} metallicity {exp} (day {day})")
-                ax.set_ylabel("Height [m]")
-                ax.set_xlabel("Latitude")
+                
+                # Set titles
+                if moment == "momentum":
+                    if comp == "meridonal":
+                        ax.set_title(f"{planet} Latitudinal gradient in the meridonal mean flow momentum flux for {metallicity} metallicity {exp} (day {day})", fontsize = 15)
+                    if comp == "vertical":
+                        ax.set_title(f"{planet} Vertical gradient in the vertical mean flow momentum flux for {metallicity} metallicity {exp} (day {day})", fontsize = 15)
+                    if comp == "total":
+                        ax.set_title(f"{planet} Sum of above components for {metallicity} metallicity {exp} (day {day})", fontsize = 20)
+                
+                if moment == "eddy":
+                    if comp == "meridonal":
+                        ax.set_title(f"{planet} Latitudinal gradient in the meridonal eddy momentum flux for {metallicity} metallicity {exp} (day {day})", fontsize = 15)
+                    if comp == "vertical":
+                        ax.set_title(f"{planet} Vertical gradient in the vertical eddy momentum flux for {metallicity} metallicity {exp} (day {day})", fontsize = 15)
+                    if comp == "total":
+                        ax.set_title(f"{planet} Sum of above components for {metallicity} metallicity {exp} (day {day})", fontsize = 20)
+                        
+                ax.set_ylabel("Height [m]", fontsize = 20)
+                ax.set_xlabel("Latitude", fontsize = 20)
                 figname = f"{planet}_{mdh}_{moment}_{day}.png"
                 
                 if limit == 0:
